23bb2cd72cb7425df1517a5b2c987252ac8732fa
sir-karabo <sir.nhlamulo@gmail.com>
2025-01-01 03:07:44 +0200

    reset

--- backend/utils/messageHandlerUtils.js
+++
@@ -1,2 +0,0 @@
-export const messageHandler = (res, message, success = true, statusCode = 200) =>
-   res.status(statusCode).send({ message, success });
--- frontend/src/utils/baseURLUtils.js
+++
@@ -1,4 +0,0 @@
-
-export const getBaseURL = () => {
-   return `http://127.0.0.1:5000`;
-}
